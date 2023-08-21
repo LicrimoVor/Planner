4746481271245640c08453494f0ba6ce628114b8
LicrimoVor <mr.licrimovor@yandex.ru>
2023-08-21 10:32:33 +0800

    bb front

--- frontend/main.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-import requests
-
-
-data = requests.api.get("http://127.0.0.1:8000/api/users/me", headers={'Authorization': 'Token 23bbf5512db220284eb3cc60cc5e5435a8a42a74'})
-
-print(data.json())
