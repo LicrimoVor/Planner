7a568dbaf4c86f1c08ad39dd71ce89dd046c9c09
LicrimoVor <mr.licrimovor@yandex.ru>
2023-09-15 01:48:40 +0800

    middleware

--- backend/backend/settings/base.py
+++ backend/backend/settings/base.py
@@ -29,19 +29,18 @@ INSTALLED_APPS = [
     'task.apps.TaskConfig',
     'organizations.apps.OrganizationsConfig',
     'api.apps.ApiConfig',
-    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'simple_history.middleware.HistoryRequestMiddleware',
+    'core.middleware.CustomMiddleware',
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True   
---
+++ backend/core/middleware.py
@@ -0,0 +1,35 @@
+class CustomMiddleware(object):
+    def __init__(self, get_response):
+        """
+        One-time configuration and initialisation.
+        """
+        self.get_response = get_response
+
+    def __call__(self, request):
+        """
+        Code to be executed for each request before the view (and later
+        middleware) are called.
+        """
+        response = self.get_response(request)
+        response["Access-Control-Allow-Origin"] = "*"
+        response["Access-Control-Allow-Methods"] = "GET, POST, DELETE"
+        response["Access-Control-Allow-Headers"] = "81.90.180.165"
+        return response
+
+    def process_view(self, request, view_func, view_args, view_kwargs):
+        """
+        Called just before Django calls the view.
+        """
+        return None
+
+    def process_exception(self, request, exception):
+        """
+        Called when a view raises an exception.
+        """
+        return None
+
+    def process_template_response(self, request, response):
+        """
+        Called just after the view has finished executing.
+        """
+        return response
