f87f09832f9cda40dfbe5ca9c109a9eb5a55d4c7
LicrimoVor <mr.licrimovor@yandex.ru>
2023-09-19 01:11:02 +0800

    .

--- backend/backend/settings/prod.py
+++ backend/backend/settings/prod.py
@@ -25,6 +25,4 @@ DATABASES = {
 }
 
 
-CSRF_TRUSTED_ORIGINS = ["http://"+x for x in os.getenv('ALLOWED_HOSTS', "127.0.0.1").split(" ")]
-print("!"*50)
-print(CSRF_TRUSTED_ORIGINS)
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', "127.0.0.1").split(" ")
