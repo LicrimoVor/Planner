804d8548a9b52aeaa8106f7763ce3045a7ef4307
LicrimoVor <mr.licrimovor@yandex.ru>
2023-10-07 14:58:45 +0800

    plz -.-

--- frontend/src/api/ajax/init.ts
+++ frontend/src/api/ajax/init.ts
@@ -1,112 +1,124 @@
 import { IAjax, IAjaxConfig } from "./interface";
 
 export const Ajax: IAjax = {
-    get: async function (config: IAjaxConfig): Promise<{}> {
-        let request_result = {success: null, fail: null};
-        
-        await $.ajax({
-            url: config.url,
-            type: "GET",
-            headers: config.token ? {Authorization: config.token} : {}
-        }).then(
-            // Success callback
-            function (callback_data) {
-                request_result.success = callback_data;
-            },
-            // Fail callback
-            function (callback_data) {
-                request_result.fail = callback_data;
-            }
-        );
+  get: async function (config: IAjaxConfig): Promise<{}> {
+    let request_result = { success: null, fail: null };
 
-        return request_result;
-    },
+    await $.ajax({
+      url: config.url,
+      type: "GET",
+      headers: config.token ? { Authorization: config.token } : {},
+    }).then(
+      // Success callback
+      function (callback_data) {
+        request_result.success = callback_data;
+      },
+      // Fail callback
+      function (callback_data) {
+        request_result.fail = callback_data;
+      }
+    );
 
-    post: async function (config: IAjaxConfig): Promise<{}> {
-        let request_result = {success: null, fail: null};
-        
-        await $.ajax({
-            url: config.url,
-            type: "POST",
-            headers: config.token ? {Authorization: config.token, "Content-Type": "application/json"} : {"Content-Type": "application/json"},
-            data: JSON.stringify(config.data)
-        }).then(
-            // Success callback
-            function (callback_data) {
-                request_result.success = callback_data;
-            },
-            // Fail callback
-            function (callback_data) {
-                request_result.fail = callback_data;
-            }
-        );
+    return request_result;
+  },
 
-        return request_result;
-    },
+  post: async function (config: IAjaxConfig): Promise<{}> {
+    let request_result = { success: null, fail: null };
 
-    delete: async function (config: IAjaxConfig): Promise<{}> {
-        let request_result = {success: null, fail: null};
-        
-        await $.ajax({
-            url: config.url,
-            type: "DELETE",
-            headers: config.token ? {Authorization: config.token, "Content-Type": "application/json"} : {"Content-Type": "application/json"},
-            data: JSON.stringify(config.data)
-        }).then(
-            // Success callback
-            function (callback_data) {
-                request_result.success = callback_data;
-            },
-            // Fail callback
-            function (callback_data) {
-                request_result.fail = callback_data;
-            }
-        );
+    await $.ajax({
+      url: config.url,
+      type: "POST",
+      headers: config.token
+        ? { Authorization: config.token, "Content-Type": "application/json" }
+        : { "Content-Type": "application/json" },
+      data: JSON.stringify(
+        Object.assign({}, config.data, {
+          csrfmiddlewaretoken: "{{ csrf_token }}",
+        })
+      ),
+    }).then(
+      // Success callback
+      function (callback_data) {
+        request_result.success = callback_data;
+      },
+      // Fail callback
+      function (callback_data) {
+        request_result.fail = callback_data;
+      }
+    );
 
-        return request_result;
-    },
+    return request_result;
+  },
 
-    put: async function (config: IAjaxConfig): Promise<{}> {
-        let request_result = {success: null, fail: null};
-        
-        await $.ajax({
-            url: config.url,
-            type: "PUT",
-            headers: config.token ? {Authorization: config.token, "Content-Type": "application/json"} : {"Content-Type": "application/json"},
-            data: JSON.stringify(config.data)
-        }).then(
-            // Success callback
-            function (callback_data) {
-                request_result.success = callback_data;
-            },
-            // Fail callback
-            function (callback_data) {
-                request_result.fail = callback_data;
-            }
-        );
+  delete: async function (config: IAjaxConfig): Promise<{}> {
+    let request_result = { success: null, fail: null };
 
-        return request_result;
-    },
+    await $.ajax({
+      url: config.url,
+      type: "DELETE",
+      headers: config.token
+        ? { Authorization: config.token, "Content-Type": "application/json" }
+        : { "Content-Type": "application/json" },
+      data: JSON.stringify(config.data),
+    }).then(
+      // Success callback
+      function (callback_data) {
+        request_result.success = callback_data;
+      },
+      // Fail callback
+      function (callback_data) {
+        request_result.fail = callback_data;
+      }
+    );
 
-    patch: async function (config: IAjaxConfig): Promise<{}> {
-        let request_result = {success: null, fail: null};
+    return request_result;
+  },
 
-        await $.ajax({
-            url: config.url,
-            type: "PATCH",
-            headers: config.token ? {Authorization: config.token, "Content-Type": "application/json"} : {"Content-Type": "application/json"},
-            data: JSON.stringify(config.data)
-        }).then(
-            // Success callback
-            function (callback_data) {
-                request_result.success = callback_data;
-            },
-            // Fail callback
-            function (callback_data) {
-                request_result.fail = callback_data;
-            }
-        );
+  put: async function (config: IAjaxConfig): Promise<{}> {
+    let request_result = { success: null, fail: null };
 
-        return request_result;
-    }
-}
+    await $.ajax({
+      url: config.url,
+      type: "PUT",
+      headers: config.token
+        ? { Authorization: config.token, "Content-Type": "application/json" }
+        : { "Content-Type": "application/json" },
+      data: JSON.stringify(config.data),
+    }).then(
+      // Success callback
+      function (callback_data) {
+        request_result.success = callback_data;
+      },
+      // Fail callback
+      function (callback_data) {
+        request_result.fail = callback_data;
+      }
+    );
+
+    return request_result;
+  },
+
+  patch: async function (config: IAjaxConfig): Promise<{}> {
+    let request_result = { success: null, fail: null };
+
+    await $.ajax({
+      url: config.url,
+      type: "PATCH",
+      headers: config.token
+        ? { Authorization: config.token, "Content-Type": "application/json" }
+        : { "Content-Type": "application/json" },
+      data: JSON.stringify(config.data),
+    }).then(
+      // Success callback
+      function (callback_data) {
+        request_result.success = callback_data;
+      },
+      // Fail callback
+      function (callback_data) {
+        request_result.fail = callback_data;
+      }
+    );
+
+    return request_result;
+  },
+};
